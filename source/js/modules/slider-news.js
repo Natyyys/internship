d54517201af108d71bcfb8b43f4295f304147e53
Наталья Моисеева <mns-fw@bk.ru>
2024-01-25 00:18:40 +0300

    fix(news): стилизует tablet

--- source/js/modules/slider-news.js
+++ source/js/modules/slider-news.js
@@ -1,5 +1,5 @@
 import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
+import { Navigation, Pagination, Grid } from 'swiper/modules';
 
 const newsSlider = document.querySelector('[data-news-slider]');
 const pagination = document.querySelector('[data-news-pagination]');
@@ -7,7 +7,7 @@ const buttonPrev = document.querySelector('[data-news-prev]');
 const buttonNext = document.querySelector('[data-news-next]');
 
 const swiper = new Swiper(newsSlider, {
-  modules: [Navigation, Pagination],
+  modules: [Navigation, Pagination, Grid],
   speed: 1500,
   loop: false,
   updateOnWindowResize: true,
@@ -16,6 +16,7 @@ const swiper = new Swiper(newsSlider, {
     0: {
       slidesPerView: 1,
       spaceBetween: 20,
+      slidesPerGroup: 1,
       grid: {
         rows: 2,
         fill: 'row',
@@ -25,14 +26,16 @@ const swiper = new Swiper(newsSlider, {
     768: {
       slidesPerView: 2,
       spaceBetween: 30,
+      slidesPerGroup: 1,
       grid: {
         rows: 2,
-        fill: 'row',
+        fill: 'column',
       },
       allowTouchMove: true,
     },
-    1200: {
+    1440: {
       slidesPerView: 'auto',
+      slidesPerGroup: 2,
       spaceBetween: 32,
       grid: {
         rows: 1,
